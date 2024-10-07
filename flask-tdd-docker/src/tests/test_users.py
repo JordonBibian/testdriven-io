64200094e5fe2255488a47300fe522cdd19b7a4c
Jordon Bibian <35698079+JordonBibian@users.noreply.github.com>
2024-10-07 14:32:25 -0700

    At RESTful Routes section

---
+++ flask-tdd-docker/src/tests/test_users.py
@@ -0,0 +1,17 @@
+# src/tests/test_users.py
+
+import json
+
+def test_add_user(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'jordon',
+            'email': 'jordon@nike.com'
+        }),
+        content_type='application/json'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert 'jordon@nike.com was added!' in data['message']
